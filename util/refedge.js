f80a6179de5f5cc7e984bf5909064f23a460cd68
Rahul Sharma <38658846+RahulSharma92@users.noreply.github.com>
2020-12-03 15:08:14 +0530

    code debugging

--- util/refedge.js
+++ util/refedge.js
@@ -4,6 +4,8 @@ const logger = require('../common/logger');
 
 module.exports = {
     saveTeamId: (conn, teamData) => {
+        console.log('team data post to sf');
+        console.dir(teamData);
         conn.apex.post('/refedge/rebot', teamData, (err, res) => {
 
             if (err) {
