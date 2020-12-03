b5682c4bdc028c2e10cb5eaa7df21c91bde78f2f
Rahul Sharma <38658846+RahulSharma92@users.noreply.github.com>
2020-12-03 10:30:18 +0530

    cheking command scope use

--- bot.js
+++ bot.js
@@ -12,28 +12,28 @@ let botCfg = {
     clientId: process.env.SLACK_CLIENT_ID,
     clientSecret: process.env.SLACK_CLIENT_SECRET,
     //update to granular scopes 
-    scopes: ['channels:join',
-        'channels:manage',
-        'channels:read',
-        'incoming-webhook',
-        'team:read',
-        'users:read',
-        'users:read.email',
+    scopes: ['channels:join',//To be able to join public channel specially created by ReferenceEdge itself.
+        'channels:manage',//To create a public channel for Customer Reference Team when the app is installed.
+        'channels:read',//To get the basic information of the channel in which ReferenceEdge is added.
+        'incoming-webhook',//To Post Message in CRP team channel
+        'team:read',//To read the Id and domain name information for the Workspace
+        'users:read',//To read the email address of the users in the Workspace.
+        'users:read.email',// To find users based on email address and send messages to them
         'users:write', 
         'channels:write',
-        'channels:history',
+        'channels:history',// To view messages of the channels in which ReferencEedge is added.
         'im:read',
         'im:write',
         'users.profile:read',
-        'im:history',
+        'im:history',//This is automatically selected and can not be removed.
         'mpim:read', 
         'mpim:write', 
         'groups:read', 
         'groups:write', 
         'groups:history',
-        'app_mentions:read',
-        'commands',
-        'chat:write'
+        'app_mentions:read',//Bot should reply when it is referenced using @rebot.
+        //'commands',
+        'chat:write'//Bot User does not reply to message with this scope.
 
     ],
     storage: mongoProvider,
